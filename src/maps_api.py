1851290ac1ad6b7de3a1d467c718867d53ab1b45
quesiu <76522766+quesiu@users.noreply.github.com>
2022-09-10 10:58:55 +0900

    feat: Add working Bing maps but transit not working in Japan?

--- src/maps_api.py
+++ src/maps_api.py
@@ -1,5 +1,6 @@
 import googlemaps
 from datetime import datetime
+import pybingmaps
 
 def get_file_contents(filename:str) -> str:
     """Given a filename,
@@ -21,16 +22,24 @@ def get_file_contents(filename:str) -> str:
 
 
 if __name__ == '__main__':
-    filename = 'gmaps_apikey'
+    # filename = 'gmaps_apikey'
+    # api_key = get_file_contents(filename)
+    # gmaps = googlemaps.Client(key=api_key)
+
+    # address1 = gmaps.reverse_geocode('35.61057793782,139.72723070545')[0]['formatted_address']
+    # address2 = gmaps.reverse_geocode('35.69435423429964,139.77095724524028')[0]['formatted_address']
+    # print('test')
+
+    # now = datetime.now()
+    # directions_result = gmaps.directions(address1,
+    #                                  address2,
+    #                                  mode="walking")
+    # print('test')
+    filename = 'bing_apikey'
     api_key = get_file_contents(filename)
-    gmaps = googlemaps.Client(key=api_key)
+    bing = pybingmaps.Bing(api_key)
 
-    address1 = gmaps.reverse_geocode('35.61057793782,139.72723070545')[0]['formatted_address']
-    address2 = gmaps.reverse_geocode('35.69435423429964,139.77095724524028')[0]['formatted_address']
-    print('test')
-
-    now = datetime.now()
-    directions_result = gmaps.directions(address1,
-                                     address2,
-                                     mode="walking")
-    print('test')
+    start = (35.61057793782, 139.72723070545)
+    end = (35.69435423429964, 139.77095724524028)
+    res = bing.route(start, end, travelMode='Transit')
+    print(res)
